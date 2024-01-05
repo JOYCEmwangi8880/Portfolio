f34001e52e49f75c085ab60e479ebf55849c7283
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-01-05 18:01:08 -0100

    upgrade portfolio

---
+++ src/details.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+
+
+
+
+
+
+
+export const details={
+    firstname:"Joyce",
+    lastname:"wangi",
+    navList:["Home","About","Skills","Work","Contact"],
+    scrollPosition:[0,545,1070,1730,3400],
+    className:["introDiv","aboutContainer","skillContainer","workDiv","contactDiv"],
+    linkedInLink:"https://www.linkedin.com/in/joyce-mwangi-470019235/",
+    githubLink:"https://github.com/JOYCEmwangi8880",
+    instagramLink:("@joyleemyles"),
+    mailLink:"joycewanguim2020@gmail.com",
+    techStack:[
+            {
+                name:"HTML",
+                img:"https://img.icons8.com/color/512/html-5--v1.png"
+            },
+            {
+                name:"CSS",
+                img:"https://img.icons8.com/fluency/512/css3.png"
+            },
+            {
+                name:"JavaScript",
+                img:"https://img.icons8.com/color/512/javascript.png"
+            },
+            
+            {
+                name:"React",
+                img:"https://img.icons8.com/office/512/react.png"
+            },
+            {
+                name:"Material UI",
+                img:"https://img.icons8.com/color/512/material-ui.png"
+            },
+           
+            {
+                name:"Node JS",
+                img:"https://img.icons8.com/fluency/512/node-js.png"
+            },
+            {
+                name:"BootStrap",
+                img:"https://img.icons8.com/color/512/bootstrap.png",
+
+            },
+            {
+                name:"Github",
+                img:"https://img.icons8.com/glyph-neue/512/github.png"
+            },
+            
+           
+
+    ],
+    projects:[
+
+       
+
+    ]
+
+}
